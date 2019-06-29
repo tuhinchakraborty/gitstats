67179f1732dd8783f86bf36c2819abf647a4ff6e
Tuhin Chakraborty <tuhin_chakraborty@outlook.com>
2019-06-29 20:47:23 +0530

    Add search term to state and modify state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,36 +1,34 @@
 import React, {Component} from 'react';
 import {TextField} from "@material-ui/core";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import Button from '@material-ui/core/Button';
 
 class Search extends Component {
-    render() {
-        const useStyles = makeStyles(theme => ({
-            container: {
-                display: 'flex',
-                flexWrap: 'wrap',
-            },
-            textField: {
-                marginLeft: theme.spacing(1),
-                marginRight: theme.spacing(1),
-            },
-            dense: {
-                marginTop: theme.spacing(2),
-            },
-            menu: {
-                width: 200,
-            },
-        }));
 
+    state = {
+        searchText: ""
+    };
+
+    handleChange = (event) => {
+        let eventTarget = event.target;
+        this.setState({
+            searchText: eventTarget.value
+        })
+    };
+
+    render() {
         return (
             <div>
                 <TextField
                     id="outlined-search"
                     label="username"
                     type="search"
-                    className={useStyles.textField}
                     margin="normal"
                     variant="outlined"
+                    onChange={event => this.handleChange(event)}
                 />
+                <Button variant="contained" color="primary">
+                    Primary
+                </Button>
             </div>
         );
     }
