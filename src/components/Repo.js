8cee1e71ba0247c45010660797dd5dab97b5c3e0
Tuhin Chakraborty <tuhin_chakraborty@outlook.com>
2019-06-30 22:00:20 +0530

    use grid to show user repos

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,13 +1,44 @@
-import React, {Component} from 'react';
+import React from 'react';
+import '../App.css';
+import {makeStyles} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-class Repo extends Component {
-    render() {
-        return (
-            <div>
-                <h3>{this.props.content.full_name}</h3>
-            </div>
-        );
-    }
-}
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        flexGrow: 0,
+        height: 100,
+        width: 300,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+}));
+
+const Repo = (props) => {
+    const classes = useStyles();
+    return (
+        <Grid container className={classes.root} spacing={5}>
+            <Grid item xs={12}>
+                <Grid container justify="center" spacing={50}>
+                    <Paper className={classes.paper}>
+                        <Typography color="textPrimary">
+                            {props.content.full_name}
+                        </Typography>
+                        <Typography color="textSecondary">
+                            {props.content.description}
+                        </Typography>
+                        <Button color="primary">Details</Button>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
 
 export default Repo;
