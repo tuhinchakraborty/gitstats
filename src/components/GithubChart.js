42c165740079affb6f29b3d38648a97019fabf6c
Tuhin Chakraborty <tuhin_chakraborty@outlook.com>
2019-07-06 15:07:12 +0530

    render the correct repo data in a chart

--- src/components/GithubChart.js
+++ src/components/GithubChart.js
@@ -1,60 +1,59 @@
 import React, {Component} from 'react';
 import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis,} from 'recharts';
 
-// const githubData = this.props.data;
-const modifiedGithubData = [];
-const convertGithubData = githubData => {
-    modifiedGithubData.push(
-        {
-            day: "sunday",
-            contributions: githubData.filter(data => data[0] === 0).reduce((acc, data) => acc + data[2], 0)
-        },
-        {
-            day: "monday",
-            contributions: githubData.filter(data => data[0] === 1).reduce((acc, data) => acc + data[2], 0)
-        },
-        {
-            day: "tuesday",
-            contributions: githubData.filter(data => data[0] === 2).reduce((acc, data) => acc + data[2], 0)
-        },
-        {
-            day: "wednesday",
-            contributions: githubData.filter(data => data[0] === 3).reduce((acc, data) => acc + data[2], 0)
-        },
-        {
-            day: "thursday",
-            contributions: githubData.filter(data => data[0] === 4).reduce((acc, data) => acc + data[2], 0)
-        },
-        {
-            day: "friday",
-            contributions: githubData.filter(data => data[0] === 5).reduce((acc, data) => acc + data[2], 0)
-        },
-        {
-            day: "saturday",
-            contributions: githubData.filter(data => data[0] === 6).reduce((acc, data) => acc + data[2], 0)
-        },
-    );
-};
-
 class GithubChart extends Component {
 
     state = {
-        githubData: []
+        modifiedGithubData: []
     };
 
     componentDidMount() {
         fetch("https://api.github.com/repos/" + this.props.fullName + "/stats/punch_card")
             .then(response => response.json())
-            .then(json => this.setState({githubData: json}))
+            .then(json => this.convertData(json))
     }
 
+    convertData = (githubData) => {
+        this.setState({
+            modifiedGithubData: [
+                {
+                    day: "sunday",
+                    contributions: githubData.filter(data => data[0] === 0).reduce((acc, data) => acc + data[2], 0)
+                },
+                {
+                    day: "monday",
+                    contributions: githubData.filter(data => data[0] === 1).reduce((acc, data) => acc + data[2], 0)
+                },
+                {
+                    day: "tuesday",
+                    contributions: githubData.filter(data => data[0] === 2).reduce((acc, data) => acc + data[2], 0)
+                },
+                {
+                    day: "wednesday",
+                    contributions: githubData.filter(data => data[0] === 3).reduce((acc, data) => acc + data[2], 0)
+                },
+                {
+                    day: "thursday",
+                    contributions: githubData.filter(data => data[0] === 4).reduce((acc, data) => acc + data[2], 0)
+                },
+                {
+                    day: "friday",
+                    contributions: githubData.filter(data => data[0] === 5).reduce((acc, data) => acc + data[2], 0)
+                },
+                {
+                    day: "saturday",
+                    contributions: githubData.filter(data => data[0] === 6).reduce((acc, data) => acc + data[2], 0)
+                }
+            ]
+        });
+    };
+
     render() {
-        convertGithubData(this.state.githubData);
         return (
             <LineChart
                 width={1000}
                 height={500}
-                data={modifiedGithubData}
+                data={this.state.modifiedGithubData}
                 margin={{
                     top: 5, right: 30, left: 20, bottom: 5,
                 }}
