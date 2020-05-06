f44851f8e4ee6c08f29552f292432617824b0484
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-05-06 21:16:11 +0530

    use CSS cards instead of material UI cards

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,12 +1,6 @@
 import React, {Component} from 'react';
 import '../App.css';
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import GithubChart from "./GithubChart";
-import {Card} from "@material-ui/core";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
 import Modal from "@material-ui/core/Modal";
 
 class Repo extends Component {
@@ -30,31 +24,25 @@ class Repo extends Component {
     render() {
         return (
             <div className="div">
-                <Grid
-                      direction="row"
-                      justify="center"
-                      alignItems="center"
-                      alignContent="center">
-                    <Grid direction="row-reverse" xs={12} sm={6} md={3}>
-                        <Card elevation={10} className="Repo">
-                            <CardHeader title={this.props.content.name}/>
-                            <CardContent>
-                                <Typography color="textSecondary">
-                                    {this.props.content.description}
-                                </Typography>
-                                <Button color="primary" onClick= {this.handleOpen}>{this.state.displayCharts ? 'Close' : 'Details'}</Button>
-                            </CardContent>
-                            <div>
-                                <Modal open={this.state.displayCharts} onClose={this.handleClose}>
-                                    <div className="modal">
-                                        <h1>{this.props.content.name}</h1>
-                                        {this.state.displayCharts ? <GithubChart fullName={this.props.content.full_name}/> : null}
-                                    </div>
-                                </Modal>
-                            </div>
-                        </Card>
-                    </Grid>
-                </Grid>
+                <div className="cards-list">
+                    <div className="card" onClick={this.handleOpen}>
+                        <div className="card_title title-slategray">
+                            <p>{this.props.content.name}</p>
+                        </div>
+                        <div className="card_description title-darkgrey">
+                            <p>{this.props.content.description}</p>
+                        </div>
+                        <div>
+                            <Modal open={this.state.displayCharts} onClose={this.handleClose}>
+                                <div className="modal">
+                                    <h1>{this.props.content.name}</h1>
+                                    {this.state.displayCharts ?
+                                        <GithubChart fullName={this.props.content.full_name}/> : null}
+                                </div>
+                            </Modal>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
